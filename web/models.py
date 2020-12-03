7184d439f5265a7b10f912695b5a64a59e1bdde0
Abolfazl Mohammadi <abolfazlmohammadi133@gmail.com>
2020-12-03 17:16:16 +0330

    start new app as web and add it's to settings.py 2

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Kharj(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    Amount = models.BigIntegerField(max_length=10)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
